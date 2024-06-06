97ba4e7db3459177c1d90c1a985d39d2515eb57c
Ansh Yadav <137482554+Ansh-Yadav-maths-lover@users.noreply.github.com>
2024-06-06 10:15:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function createRoom() {
     let roomName = prompt("Enter room name:");
     if (roomName) {
+        let password = prompt("Enter room password:");
         let roomId = generateRoomId();
         let roomLink = window.location.origin + "/chat.html?room=" + roomId;
         let roomListItem = document.createElement("li");
         roomListItem.innerHTML = `<a href="${roomLink}" target="_blank">${roomName}</a>`;
         document.getElementById("rooms").appendChild(roomListItem);
+        localStorage.setItem(roomId, JSON.stringify({ password: password, messages: [] }));
     }
 }
 
